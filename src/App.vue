da3e11912f0a2aa43678123a6484cee0a355bae7
Bran Dale Nacario <nacariodale@gmail.com>
2024-04-03 17:10:12 +0800

    feat: brands section done with grid layout and logos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import { ref } from "vue";
+import { register } from "swiper/element/bundle";
 
-let isNavOpen = ref(true);
+register();
+
+let isNavOpen = ref(false);
 const triggerNav = () => {
   isNavOpen.value = !isNavOpen.value;
 };
@@ -23,7 +26,7 @@ const triggerNav = () => {
           <div class="cursor-pointer lg:hidden">
             <i
               @click="triggerNav"
-              class="ri-menu-3-line text-primary text-4xl"
+              class="ri-menu-3-line text-4xl text-primary"
             ></i>
           </div>
           <ul
@@ -52,11 +55,11 @@ const triggerNav = () => {
     <div class="mx-auto max-w-[1920px] overflow-hidden bg-white">
       <!-- GRID LAYOUT IMG -->
       <div
-        class="xl:bg-grid fixed bottom-0 left-0 right-0 top-0 z-10 xl:bg-center xl:bg-repeat-y"
+        class="fixed bottom-0 left-0 right-0 top-0 z-10 xl:bg-grid xl:bg-center xl:bg-repeat-y"
       ></div>
       <!-- HERO BG IMAGE -->
       <section
-        class="hero bg-hero relative z-20 h-[640px] bg-fixed bg-center bg-no-repeat lg:bg-cover xl:h-[840px] xl:rounded-bl-[290px]"
+        class="hero relative z-20 h-[640px] bg-hero bg-fixed bg-center bg-no-repeat lg:bg-cover xl:h-[840px] xl:rounded-bl-[290px]"
       >
         <div class="container mx-auto flex h-full items-center">
           <div
@@ -80,9 +83,252 @@ const triggerNav = () => {
         <div class="container mx-auto">
           <!-- GRID ITEMS -->
           <div class="grid grid-cols-1 gap-12 xl:grid-cols-3">
-            <div class="steps__step bg-pink-200 text-center">
-              <div></div>
+            <!-- GRID ITEM 1 -->
+            <div class="steps__step text-center">
+              <div class="mb-4">
+                <i class="ri-phone-line text-6xl text-accent"></i>
+              </div>
+              <h3 class="h3 mb-5">Book now, Walk-in Welcome</h3>
+              <p class="mx-auto mb-5 max-w-md">
+                Contact us to secure your spot, pick your preferred schedule,
+                and choose from our diverse selection of working spaces. Walk in
+                anytime and we'll gladly accommodate you with the working spaces
+                available
+              </p>
+              <a href="#" class="flex justify-center gap-2 font-medium"
+                >Read More
+                <i class="ri-arrow-right-line text-accent"></i>
+              </a>
             </div>
+            <!-- GRID ITEM 2 -->
+            <div class="steps__step text-center">
+              <div class="mb-4">
+                <i class="ri-bard-line text-6xl text-accent"></i>
+              </div>
+              <h3 class="h3 mb-5">Flexible Services, Fresh Amenities</h3>
+              <p class="mx-auto mb-5 max-w-md">
+                Experience our flexible services and enjoy a fresh, clean
+                environment with complimentary food and drinks. Whether you need
+                a quick snack or a caffeine boost, we've got you covered.
+              </p>
+              <a href="#" class="flex justify-center gap-2 font-medium"
+                >Read More
+                <i class="ri-arrow-right-line text-accent"></i>
+              </a>
+            </div>
+            <!-- GRID ITEM 3 -->
+            <div class="steps__step text-center">
+              <div class="mb-4">
+                <i class="ri-service-line text-6xl text-accent"></i>
+              </div>
+              <h3 class="h3 mb-5">Flexible Stay Options</h3>
+              <p class="mx-auto mb-5 max-w-md">
+                Enjoy the freedom to extend your stay in our working space
+                anytime or cancel as needed. We're here to accommodate your
+                schedule and preferences to ensure the best work environment
+              </p>
+              <a href="#" class="flex justify-center gap-2 font-medium"
+                >Read More
+                <i class="ri-arrow-right-line text-accent"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- ABOUT -->
+      <section class="about relative z-20 mt-[200px] xl:mt-[200px]">
+        <div class="container mx-auto xl:px-0">
+          <div
+            class="flex flex-col items-center justify-between gap-8 text-center xl:flex-row xl:gap-[74px] xl:text-left"
+          >
+            <!-- TEXT  -->
+            <div
+              class="order-2 flex max-w-xl flex-1 flex-col items-center gap-8 xl:order-none xl:max-w-[410px] xl:items-start"
+            >
+              <h2 class="h2">Elevating Workspace Experiences, Seamlessly</h2>
+              <p>
+                Step into GeekHub, where the fusion of comfort, productivity,
+                and innovation transforms the way you work. Our meticulously
+                designed spaces blend functionality with aesthetics to inspire
+                creativity and drive success. With dedicated staff ensuring
+                unparalleled service and fully equipped workspaces tailored to
+                your needs, GeekHub empowers you to thrive, one task at a time.
+              </p>
+              <!-- PHONE -->
+              <div
+                class="flex items-center justify-center gap-4 xl:justify-start"
+              >
+                <div
+                  class="flex h-[60px] w-[60px] items-center justify-center rounded-full bg-accent/15"
+                >
+                  <i class="ri-phone-fill text-3xl text-accent"></i>
+                </div>
+                <div class="text-left">
+                  <div class="text-2xl font-bold">0926 003 2485</div>
+                  <div>Call Us Anytime</div>
+                </div>
+              </div>
+              <button class="btn btn-primary mx-auto xl:mx-0">
+                Ask Us A Question
+                <i class="ri-arrow-right-line text-accent"></i>
+              </button>
+            </div>
+            <!-- IMAGE -->
+            <div
+              class="about__img order-1 mx-auto max-w-[453px] xl:order-none xl:mx-0 xl:max-w-none"
+            >
+              <img
+                src="/src/assets/about/about.jpg"
+                class="rounded-bl-[80px] rounded-tr-[280px]"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- TESTIMONIAL -->
+      <section class="testimonial relative z-20 mt-[200px] xl:mt-[200px]">
+        <div
+          class="testimonial__bg container mx-auto w-full rounded-[70px] bg-accent-secondary px-6"
+        >
+          <div class="flex flex-col items-center pb-[20px] pt-[40px]">
+            <h2 class="testimonial__title h2 mb-9 text-center">
+              What clients say
+            </h2>
+          </div>
+          <!-- SLIDER -->
+          <swiper-container
+            class="h-[400px] pb-3"
+            :slides-per-view="1"
+            :space-between="20"
+            :centered-slides="false"
+            :pagination="{
+              hideOnClick: true,
+            }"
+            :breakpoints="{
+              320: {
+                slidesPerView: 1,
+              },
+              960: {
+                slidesPerView: 2,
+              },
+              1200: {
+                slidesPerView: 3,
+              },
+            }"
+          >
+            <!-- slides -->
+            <swiper-slide>
+              <div
+                class="testimonial__item mx-auto flex h-[340px] w-full max-w-[450px] flex-col items-center justify-center rounded-[30px] bg-white p-9"
+              >
+                <div class="mb-6 flex gap-4">
+                  <img
+                    src="../src/assets/avatars/avatar2.jpg"
+                    class="h-[100px] rounded-[80px]"
+                  />
+                  <div>
+                    <h3 class="h3 font-bold">Wendy Bustamante</h3>
+                    <div>Gokayama, Japan</div>
+                  </div>
+                </div>
+                <p>
+                  Lorem Ipsum is simply dummy text of the typesetting industry.
+                </p>
+              </div>
+            </swiper-slide>
+
+            <swiper-slide>
+              <div
+                class="testimonial__item mx-auto flex h-[340px] w-full max-w-[450px] flex-col items-center justify-center rounded-[30px] bg-white p-9"
+              >
+                <div class="mb-6 flex gap-4">
+                  <img
+                    src="../src/assets/avatars/avatar3.jpg"
+                    class="h-[100px] rounded-[80px]"
+                  />
+                  <div>
+                    <h3 class="h3 font-bold">Gerald Bernardo</h3>
+                    <div>Manila, Philippines</div>
+                  </div>
+                </div>
+                <p>
+                  Lorem Ipsum is simply dummy text of the typesetting industry.
+                </p>
+              </div>
+            </swiper-slide>
+
+            <swiper-slide>
+              <div
+                class="testimonial__item mx-auto flex h-[340px] w-full max-w-[450px] flex-col items-center justify-center rounded-[30px] bg-white p-9"
+              >
+                <div class="mb-6 flex gap-4">
+                  <img
+                    src="../src/assets/avatars/avatar1.jpg"
+                    class="h-[100px] rounded-[80px]"
+                  />
+                  <div>
+                    <h3 class="h3 font-bold">Pearl Aljas Jr.</h3>
+                    <div>Cebu City, Philippines</div>
+                  </div>
+                </div>
+                <p>
+                  Lorem Ipsum is simply dummy text of the typesetting industry.
+                </p>
+              </div>
+            </swiper-slide>
+
+            <swiper-slide>
+              <div
+                class="testimonial__item mx-auto flex h-[340px] w-full max-w-[450px] flex-col items-center justify-center rounded-[30px] bg-white p-9"
+              >
+                <div class="mb-6 flex gap-4">
+                  <img
+                    src="../src/assets/avatars/avatar4.jpg"
+                    class="h-[100px] rounded-[80px]"
+                  />
+                  <div>
+                    <h3 class="h3 font-bold">Troy Michael</h3>
+                    <div>Heidelberg, Germany</div>
+                  </div>
+                </div>
+                <p>
+                  Lorem Ipsum is simply dummy text of the typesetting industry.
+                </p>
+              </div>
+            </swiper-slide>
+          </swiper-container>
+        </div>
+      </section>
+      <!-- BRANDS -->
+      <section class="brands relative z-20 mt-[200px] xl:mt-[200px]">
+        <div class="container mx-auto">
+          <div
+            class="grid grid-cols-1 items-center justify-between gap-4 lg:grid-cols-3"
+          >
+            <img
+              class="brands__img mx-auto block h-[200px] w-full max-w-[200px]"
+              src="/src/assets/brands/brand1.png"
+            />
+            <img
+              class="brands__img mx-auto block h-[200px] w-full max-w-[200px]"
+              src="/src/assets/brands/brand5.png"
+            />
+            <img
+              class="brands__img mx-auto block h-[200px] w-full max-w-[200px]"
+              src="/src/assets/brands/brand2.png"
+            />
+            <img
+              class="brands__img mx-auto block h-[200px] w-full max-w-[200px]"
+              src="/src/assets/brands/brand3.png"
+            />
+            <img
+              class="brands__img mx-auto block h-[200px] w-full max-w-[200px]"
+              src="/src/assets/brands/brand4.png"
+            />
+            <img
+              class="brands__img mx-auto block h-[200px] w-full max-w-[200px]"
+              src="/src/assets/brands/brand6.png"
+            />
           </div>
         </div>
       </section>
