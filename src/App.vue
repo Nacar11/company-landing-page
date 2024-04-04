3d30bf3dbb1e1a5bb8698336520b7e466bdf10b7
Bran Dale Nacario <nacariodale@gmail.com>
2024-04-04 23:25:13 +0800

    feat: image location testing

--- src/App.vue
+++ src/App.vue
@@ -508,12 +508,12 @@ const triggerNav = () => {
           </div>
           <div class="news__grid grid grid-cols-1 gap-[27px] xl:grid-cols-3">
             <NewsCard
-              :image="'../src/assets/news/news2.jpg'"
+              :image="'../../src/assets/news/news2.jpg'"
               title="Partnership with Branded and Caffeinated"
               subtitle="21 June, 2021"
             ></NewsCard>
             <NewsCard
-              :image="'../src/assets/news/news2.jpg'"
+              :image="'././src/assets/news/news3.jpg'"
               title="Student Promos: Latest Affordable Working Spaces for students "
               subtitle="4 March, 2022"
             ></NewsCard>
